f4dfab83cafc00bd715d050d41bfdeee3e6af57c
kauamassei <gameplayultra11@gmail.com>
2025-03-25 17:26:39 -0300

    feat: add useStates and div app-container with form, input and button

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,14 +1,33 @@
-import {useEffect, useState} from 'react'
-import "./TodoApp.css"
+import { useEffect, useState } from "react";
+import "./TodoApp.css";
 
 const TodoApp = () => {
+  const [todos, setTodos] = useState([]);
 
+  const [inputValue, setInputValue] = useState("");
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <>
- 
-    </>
-  )
-}
+      <div className="app-container">
+        <h1 className="title">Lista de tarefas</h1>
+        {/* forms */}
+        <form onSubmit={handleSubmit} className="form-container">
+          <input className="input-field"
+            type="text"
+            placeholder="Digite uma tarefa..."
+            value={inputValue}
+            onChange={(e) => setInputValue(e.target.value)}
+          />
+          <button type="submit" className="add-button">Adicionar</button>
+        </form>
 
-export default TodoApp
+        {todos.length === 0 && <p className="empty">Não há tarefas</p>}
+      </div>
+    </>
+  );
+};
+
+export default TodoApp;
